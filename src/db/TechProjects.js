147cdb6774f4c8839b014f813e93d44fe39d2922
Tali Scheer <44593008+tzip25@users.noreply.github.com>
2019-06-20 10:47:19 -0400

    updated project descriptions

--- src/db/TechProjects.js
+++ src/db/TechProjects.js
@@ -7,7 +7,7 @@ const projects = [
     backend: "",
     technologies: "React • Redux • Rails • JWT • Custom CSS • Semantic UI React",
     api: "Clarifai API • TMDB API",
-    description: "Shazam for faces! Capture celebrity photos mid-movie to see an actor’s name, filmography, a link to their IMDB and more.",
+    description: "Shazam for faces! Capture celebrity photos mid-movie to see an actor’s name, filmography, a link to their IMDB and more. Now that's smart.",
     img: "whodat.png"
   },
   {
@@ -18,7 +18,7 @@ const projects = [
     backend: "",
     technologies: "Vanilla JS • Rails • Custom CSS • Semantic UI",
     api: "-",
-    description: "A game for word enthusiasts. Form words by typing letters that you see on the board. Keep playing until the board fills up.",
+    description: "A game for word enthusiasts. Form words by typing letters that you see on the board. Keep playing until the board fills up. Carefull, it's addictive!",
     img: "wwn.png"
   },
   {
@@ -29,7 +29,7 @@ const projects = [
     backend: "",
     technologies: "React • Rails • JWT • Custom CSS • Semantic UI React",
     api: "Kiwi API",
-    description: "Find unique destinations within your budget. Search for flights using only a start location, date, and maximum ticket price.",
+    description: "Find unique destinations within your budget. Search for flights using only a start location, date, and maximum ticket price. Go explore!",
     img: "du.png"
   },
   {
@@ -40,7 +40,7 @@ const projects = [
     backend: "",
     technologies: "Ruby on Rails • Custom CSS • Semantic UI",
     api: "-",
-    description: "Social media for foodies! Find and connect with other food fans. Match based on food preferences, chat and more.",
+    description: "Social media for foodies! Find and connect with other food fans. Match based on food preferences, chat and more. Getting hungry?",
     img: "fyf.png"
   },
 ]
