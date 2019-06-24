4a09746522067d2b72546bc665135163957177d4
Tali Scheer <44593008+tzip25@users.noreply.github.com>
2019-06-24 14:15:41 -0400

    added onClick to design projects

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,11 +2,41 @@ import React from 'react';
 
 class GalleryItem extends React.Component {
 
+  state = {
+      isClicked: false
+    }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }))
+  }
+
+  leaveItem = () => {
+    this.setState({
+      isClicked: false
+    })
+  }
+
   render(){
     const { project } = this.props
     return(
-      <div className="galleryItem">
-          < img src={project.imgUrl} className="galerryImg" alt={`${project.name} ${project.type}`}/>
+      <div
+        className={this.state.isClicked ? "galleryItemClicked" : "galleryItem"}
+        onClick={this.handleClick}
+        onMouseLeave={this.leaveItem}
+      >
+        {
+          this.state.isClicked ?
+          <div className="designProjectText">
+            <h2>{project.type}</h2>
+            <p>{project.description}</p>
+            <img src={project.imgUrl} className="galerryHoverImg" alt={`${project.name} ${project.type}`}/>
+
+          </div>
+          :
+          <img src={project.imgUrl} className="galerryImg" alt={`${project.name} ${project.type}`}/>
+        }
       </div>
     )
   }
--- src/db/DesignProjects.js
+++ src/db/DesignProjects.js
@@ -1,23 +1,59 @@
 const designProjects = [
   {
-    type: "Logo",
+    type: "Print Design",
+    software: ["Adobe Illustrator", "Adobe Photoshop"],
+    name: "Black Plaid",
+    imgUrl: "black-plaid.png",
+    description: "Deisgned a black plaid print for brand's new product development."
+  },
+  {
+    type: "Logo Design",
+    software: "Adobe Illustrator",
     name: 'The Creators',
-    imgUrl: "creators.png"
+    imgUrl: "creators.png",
+    description: "Deisgned logo for private client. Incorporated paint brush, light bulb, and flame."
   },
   {
-    type: "Logo",
+    type: "Logo Design",
+    software: "Adobe Illustrator",
     name: "Baby K'tan",
-    imgUrl: "babyktan1.png"
+    imgUrl: "babyktan1.png",
+    description: "Refreshed brand logo to more modern look while keeping with brand font and style."
   },
   {
-    type: "Logo",
+    type: "Logo Design",
+    software: "Adobe Illustrator",
     name: "Glass Cocktail Bar",
-    imgUrl: "glass.png"
+    imgUrl: "glass.png",
+    description: "Deisgned cocktail bar logo for private client."
   },
   {
-    type: "Logo",
+    type: "Print Design",
+    software: ["Adobe Illustrator", "Adobe Photoshop"],
+    name: "Arrow",
+    imgUrl: "arrow-print.png",
+    description: "Deisgned a classic arrow print for brand's new product development."
+  },
+  {
+    type: "Vector Design",
+    software: ["Adobe Illustrator"],
+    name: "Avatar",
+    imgUrl: "tali-large.png",
+    description: "Deisgned vector self-portrait for use as avatar."
+  },
+  {
+    type: "Print Design",
+    software: ["Adobe Illustrator", "Adobe Photoshop"],
+    name: "Pink Plaid",
+    imgUrl: "pink-plaid.png",
+    description: "Deisgned a pink plaid print for brand's new product development."
+  },
+  {
+    type: "Logo Design",
+    software: "Adobe Illustrator",
     name: "WhoDat",
-    imgUrl: "whodat-logo.png"
+    imgUrl: "whodat-logo.png",
+    description: "Deisgned logo for private client. Incorporated paint brush, light bulb, and flame."
   },
 ]
 
